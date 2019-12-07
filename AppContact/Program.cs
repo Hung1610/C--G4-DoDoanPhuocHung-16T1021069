2f7682d919500d387560c53da90cb3ffa2cc014a
Hung1610 <phuochung008@gmail.com>
2019-12-07 14:40:36 +0700

    fix path

---
+++ AppContact/Program.cs
@@ -0,0 +1,25 @@
+﻿using AppContact.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppContact
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmThongTinSinhVien("16T1021165"));
+            //Application.Run(new frmContact());
+            Application.Run(new frmLogin());
+        }
+    }
+}
